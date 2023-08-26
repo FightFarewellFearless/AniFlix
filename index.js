e8dedc6898fcadf7e3a086ba175a74026fbb5edf
FightFarewellFearless <faizakbar088@gmail.com>
2023-08-26 11:11:04 +0700

    remove unnecessary ignore log

--- index.js
+++ index.js
@@ -2,11 +2,9 @@
  * @format
  */
 
-import { AppRegistry, LogBox } from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 require('moment/locale/id');
-
-LogBox.ignoreLogs(['new NativeEventEmitter()']);
 
 AppRegistry.registerComponent(appName, () => App);
