ed9227ddc066ff5118489b2346e6d8cc05ada30c
FightFarewellFearless <faizakbar088@gmail.com>
2023-09-24 17:45:56 +0700

    automatic update installation (improve)

--- src/component/NeedUpdate.tsx
+++ src/component/NeedUpdate.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -24,6 +24,21 @@ function NeedUpdate(props: Props) {
   const markdownElement = useMarkdown(props.route.params.changelog, {
     colorScheme: colorScheme,
   });
+
+  const [isDownloadStart, setIsDownloadStart] = useState(false);
+  const [downloadProgress, setDownloadProgress] = useState(0);
+
+  useEffect(() => {
+    RNFetchBlob.fs
+      .exists(
+        `/storage/emulated/0/Download/AniFlix-${props.route.params.latestVersion}.apk`,
+      )
+      .then(value => {
+        setIsDownloadStart(value);
+        setDownloadProgress(value ? 100 : 0);
+      });
+  }, [props.route.params.latestVersion]);
+
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
@@ -48,40 +63,69 @@ function NeedUpdate(props: Props) {
             <Fragment key={`changelog_${i}`}>{el}</Fragment>
           ))}
         </ScrollView>
-        <TouchableOpacity
-          style={styles.download}
-          onPress={() => {
-            ToastAndroid.show('Mendownload update...', ToastAndroid.SHORT);
-            Alert.alert(
-              'Perhatian',
-              'Selama proses download, tolong jangan keluar aplikasi untuk menghindari kesalahan',
-            );
-            RNFetchBlob.config({
-              addAndroidDownloads: {
-                useDownloadManager: true,
+        {!isDownloadStart ? (
+          <TouchableOpacity
+            style={styles.download}
+            onPress={() => {
+              setIsDownloadStart(true);
+              ToastAndroid.show('Mendownload update...', ToastAndroid.SHORT);
+              Alert.alert(
+                'Perhatian',
+                'Selama proses download, tolong jangan keluar aplikasi untuk menghindari kesalahan',
+              );
+              RNFetchBlob.config({
                 path: `/storage/emulated/0/Download/AniFlix-${props.route.params.latestVersion}.apk`,
-                title: 'Sedang memperbarui',
-                description: 'Pembaruan aplikasi',
-                mime: 'application/vnd.android.package-archive',
-                mediaScannable: true,
-                notification: true,
-              },
-            })
-              .fetch('GET', props.route.params.download)
-              .then(async res => {
-                await RNFetchBlob.android.actionViewIntent(
-                  res.path(),
-                  'application/vnd.android.package-archive',
-                );
-              });
-          }}>
-          <Icon
-            name="file-download"
-            color={globalStyles.text.color}
-            size={20}
-          />
-          <Text style={globalStyles.text}>Download update</Text>
-        </TouchableOpacity>
+              })
+                .fetch('GET', props.route.params.download)
+                .progress((received, total) => {
+                  setDownloadProgress(Math.floor((received / total) * 100));
+                })
+                .then(async res => {
+                  setIsDownloadStart(true);
+                  setDownloadProgress(100);
+                  await RNFetchBlob.android.actionViewIntent(
+                    res.path(),
+                    'application/vnd.android.package-archive',
+                  );
+                })
+                .catch(() => {
+                  setIsDownloadStart(false);
+                  ToastAndroid.show('Download gagal!', ToastAndroid.SHORT);
+                });
+            }}>
+            <Icon
+              name="file-download"
+              color={globalStyles.text.color}
+              size={20}
+            />
+            <Text style={globalStyles.text}>Download update</Text>
+          </TouchableOpacity>
+        ) : downloadProgress === 100 ? (
+          <TouchableOpacity
+            style={styles.download}
+            onPress={() => {
+              RNFetchBlob.android.actionViewIntent(
+                `/storage/emulated/0/Download/AniFlix-${props.route.params.latestVersion}.apk`,
+                'application/vnd.android.package-archive',
+              );
+            }}>
+            <Icon name="download" color={globalStyles.text.color} size={20} />
+            <Text style={globalStyles.text}>Instal update</Text>
+          </TouchableOpacity>
+        ) : (
+          <View style={styles.download}>
+            <View style={styles.sliderContainer}>
+              <View
+                style={[
+                  styles.slider,
+                  {
+                    width: `${downloadProgress}%`,
+                  },
+                ]}
+              />
+            </View>
+          </View>
+        )}
       </View>
     </View>
   );
@@ -133,6 +177,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#008b13',
     borderRadius: 5,
     justifyContent: 'center',
+  },
+  sliderContainer: {
+    width: '100%',
+    backgroundColor: 'gray',
+    height: 20,
+    borderRadius: 5,
+  },
+  slider: {
+    height: 20,
+    borderRadius: 5,
+    backgroundColor: 'blue',
   },
 });
 
