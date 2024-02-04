87da4a878b1a939cc5880220e32cc93b7359a8e2
FightFarewellFearless <faizakbar088@gmail.com>
2024-02-04 10:31:27 +0700

    Chat (Beta)

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 /**
  * @format
  */
-import { polyfill as polyfillEncoding } from 'react-native-polyfill-globals/src/encoding';
-import { polyfill as polyfillReadableStream } from 'react-native-polyfill-globals/src/readable-stream';
-import { polyfill as polyfillFetch } from 'react-native-polyfill-globals/src/fetch';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 require('moment/locale/id');
-
-polyfillEncoding();
-polyfillReadableStream();
-polyfillFetch();
 
 AppRegistry.registerComponent(appName, () => App);
--- src/component/Home/Chat.tsx
+++ src/component/Home/Chat.tsx
@@ -1,159 +1,147 @@
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { HomeNavigator } from "../../types/navigation";
-import { View, Text, TouchableOpacity, ToastAndroid } from "react-native";
+import { View, Text, TouchableOpacity, ToastAndroid, ScrollView, TextInput, StyleSheet } from "react-native";
 import { useCallback, useMemo, useRef, useState } from "react";
-import { useMarkdown } from "react-native-marked";
+import gpti from 'gpti';
+import Markdown, { useMarkdown } from "react-native-marked";
 import colorScheme from "../../utils/colorScheme";
-import { ScrollView } from "react-native";
-import { TextInput } from "react-native";
 import throttle from "../../utils/throttleFunction";
-import globalStyles from "../../assets/style";
+import globalStyles, { lightText } from "../../assets/style";
+import { FlashList } from "@shopify/flash-list";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const defaultMessages = [{
+  content: 'SISTEM: Kamu merespon menggunakan markdown',
+  role: 'user',
+}, {
+  content: 'SISTEM: Kamu adalah AniFlix Chat, sebuah aplikasi yang dibuat oleh FightFarewellFearless (Pirles).',
+  role: 'user'
+}, {
+  content: "Saya adalah AniFlix Chat, saya siap menjawab berbagai pertanyaan anda!",
+  role: 'assistant'
+}] as const;
 
 type Props = BottomTabScreenProps<HomeNavigator, "Chat">
+
+interface Message {
+  content: string;
+  role: 'user' | 'assistant';
+}
+
 function Chat(props: Props) {
 
-  let messages = [
-    {
-      role: "assistant",
-      content: "Hello! How can I help you today? 😊"
-    },
-    {
-      role: "user",
-      content: "Hi, tell me the names of the movies released in 2023."
-    },
-    {
-      role: "assistant",
-      content: "Certainly! Here are some movies that were released in 2023:\n\n1.  **About My Father** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n2.  **The Little Mermaid** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n3.  **Fast X** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n4.  **Spider-Man: Across the Spider-Verse** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n5.  **The Machine** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n6.  **Book Club: The Next Chapter** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n7.  **Guardians of the Galaxy Vol. 3** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n8.  **John Wick: Chapter 4** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n9.  **Are You There God? It's Me, Margaret** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n10.  **Evil Dead Rise** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n11.  **The Super Mario Bros. Movie** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n12.  **Love Again** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n13.  **Kandahar** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n14.  **Dungeons & Dragons: Honor Among Thieves** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n15.  **Shin Kamen Rider** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n16.  **Knights of the Zodiac** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n17.  **The Pope's Exorcist** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n18.  **Shazam! Fury of the Gods** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n19.  **All That Breathes** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n20.  **Sailor Moon Cosmos** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n21.  **Hypnotic** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n22.  **Sound of Freedom** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n23.  **The Boogeyman** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n24.  **Chicken Run: Dawn of the Nugget** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n25.  **A Lot of Nothing** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n26.  **Followers** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n27.  **Big George Foreman** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n28.  **Asterix & Obelix: The Middle Kingdom** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n29.  **Ant-Man and the Wasp: Quantumania** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n30.  **Transformers: Rise of the Beasts** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n31.  **Follow Her** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n32.  **Prom Pact** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n33.  **God Is a Bullet** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n34.  **Still: A Michael J. Fox Movie** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n35.  **Nefarious** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n36.  **Nanny Dearest** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n37.  **Monica** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n38.  **Wild Life** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n39.  **Palm Trees and Power Lines** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n40.  **What's Love Got to Do with It?** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n41.  **Creed III** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n42.  **One True Loves** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n43.  **BlackBerry** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n44.  **Suzume** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n45.  **Rock Dog 3: Battle the Beat** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n46.  **Gridman Universe** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n47.  **Digimon Adventure 02: The Beginning** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n48.  **Woman of the Photographs** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n49.  **El Tonto** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n50.  **Seriously Red** [^1^](https://editorial.rottentomatoes.com/guide/best-movies-of-2023/)\n\nI hope this helps! Let me know if you have any other questions."
-    }
-  ];
+  const flashList = useRef<FlashList<Message>>();
 
-  const streamChat = useCallback((prompt: string, callback: (text: string | null) => void) => {
-    fetch("https://nexra.aryahcr.cc/api/chat/complements", {
-      //@ts-ignore
-      reactNative: { textStreaming: true },
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        messages: [...messages, {
-          role: "user",
-          content: prompt
-        }],
-        conversation_style: "Balanced",
-        markdown: false,
-        stream: true,
-        model: "Bing"
-      })
-    }).then(response => {
-      const reader = response.body?.getReader();
-      console.log(reader)
-      let tmp: string | null = '';
-      let err: Error | null = null;
+  const [messagesHistroy, setMessagesHistroy] = useState<Message[]>([]);
+  const [messageLoading, setMessageLoading] = useState(false);
 
-      reader?.read().then(function processText({ done, value }) {
-        if (done) {
-          if (err != null) {
-            console.log(err);
-          } else {
-            // console.log('end');
-          }
-          return;
-        }
-
-        const chunk = new TextDecoder().decode(value);
-        const chk = chunk.toString().split('');
-
-        chk.forEach(part => {
-          if (err === null) {
-            let result = null;
-            let convert = "";
-
-            try {
-              convert = JSON.parse(part);
-              result = part;
-              tmp = null;
-            } catch (e) {
-              if (tmp === null) {
-                tmp = part;
-              } else {
-                try {
-                  convert = JSON.parse(tmp);
-                  result = tmp;
-                  tmp = null;
-                } catch (e) {
-                  tmp = tmp + part;
-                  try {
-                    convert = JSON.parse(tmp);
-                    result = tmp;
-                    tmp = null;
-                  } catch (e) {
-                    tmp = tmp;
-                  }
-                }
-              }
-            }
-
-            if (result !== null) {
-              const jsonRes = JSON.parse(result);
-              if (jsonRes.code === undefined && jsonRes.status === undefined) {
-                callback(jsonRes.message as string | null);
-                console.log(jsonRes)
-              } else {
-                err = jsonRes;
-              }
-            }
-          }
-        });
-
-        return reader.read().then(processText);
-      }).catch((err) => {
-        ToastAndroid.show(err.stack, ToastAndroid.SHORT);
-      });
-    }).catch((err) => {
-      ToastAndroid.show(err.stack, ToastAndroid.SHORT);
-    });
-  }, []);
+  const streamChat = useCallback((prompt: string, callback: (text: string | undefined) => void) => {
+    gpti.gpt({
+      messages: [...defaultMessages, ...messagesHistroy],
+      markdown: false,
+      model: 'gpt-4',
+      prompt,
+    }, (err, data) => {
+      callback(data?.gpt);
+    })
+  }, [messagesHistroy]);
 
   const prompt = useRef('');
-  const [text, setText] = useState<string>('');
-
-  const updateThrottle = throttle((text: string | null) => {    
-      if(text!==null){
-        setText(text);
-      }
-  }, 600);
 
   const tanya = useCallback(() => {
-    console.log(prompt.current)
+    if(prompt.current.trim() === '') {
+      return;
+    }
+    const delayScrollToEnd = () => setTimeout(() => {
+      flashList.current?.scrollToEnd();
+    }, 50);
+
+    setMessagesHistroy(old => [...old, { content: prompt.current, role: 'user' }]);
+    delayScrollToEnd();
     streamChat(prompt.current, (text) => {
-      if(text!==null){
-        setText(text);
-      }
-    })
+      setMessagesHistroy(old => [...old, { content: text ?? 'TERJADI ERROR: Pastikan kamu terhubung ke internet dan coba lagi', role: 'assistant' }]);
+      delayScrollToEnd();
+      setMessageLoading(false);
+    });
+    setMessageLoading(true);
   }, []);
 
-  const markdown = useMarkdown(text, {
-    colorScheme: colorScheme,
-  });
-
   return (
-    // <View>
-    //   <ScrollView>
-    //     {markdown}
-    //   </ScrollView>
-    //   <TextInput onChangeText={t => {
-    //     prompt.current = t;
-    //   }} />
-    //   <TouchableOpacity onPress={tanya}>
-    //     <Text>Tanya</Text>
-    //   </TouchableOpacity>
-    // </View>
-    <Text style={[globalStyles.text, {
-      fontWeight: 'bold',
-      fontSize: 20,
-      alignSelf: 'center',
-    }]}>Coming soon!</Text>
+    <View style={{ flex: 1 }}>
+      <FlashList
+        // @ts-ignore
+        ref={flashList}
+        ListHeaderComponent={
+          <View style={styles.center}>
+            <Text style={[globalStyles.text, { fontSize: 30 }]}>AniFlix</Text>
+            <Icon name="comments" size={37} color="orange" />
+            <Text style={[globalStyles.text, { fontSize: 30, fontWeight: 'bold' }]}>Chat (Beta)</Text>
+            <Text style={[globalStyles.text, { fontSize: 15, textAlign: 'center' }]}>AniFlix Chat adalah fitur chat yang ada di AniFlix bertujuan 
+            untuk memberikan pengalaman yang berbeda kepada pengguna.</Text>
+            <Text style={[globalStyles.text, { fontSize: 15, textAlign: 'center' }]}>Note: AI tidak terbatas pada informasi seputar anime. Kamu bisa menanyakan apa saja ke AI.
+             Dan juga tolong diingat bahwa informasi yang diberikan tidak terbaru dan mungkin salah</Text>
+          </View>
+        }
+        data={messagesHistroy}
+        renderItem={({ item }) => {
+          return (
+            <View style={[item.role === 'user' ? styles.userResponse : styles.assistantResponse, { marginVertical: 5, maxWidth: '80%' }]}>
+              <CustomMarkdown content={item.content} />
+            </View>
+          )
+        }} />
+      <View style={{ flexDirection: 'row' }}>
+        <TextInput placeholder="Ketik sesuatu..." style={styles.textInput} multiline onChangeText={t => {
+          prompt.current = t;
+        }} />
+        <TouchableOpacity style={styles.button} disabled={messageLoading} onPress={tanya}>
+          <Text style={{ color: lightText }}>{messageLoading ? 'Loading...' : 'Kirim'}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   )
 }
+
+function CustomMarkdown({ content }: { content: string }) {
+  const markdown = useMarkdown(content, {
+    colorScheme,
+  });
+  return markdown;
+}
+
+const styles = StyleSheet.create({
+  textInput: {
+    backgroundColor: colorScheme === 'dark' ? '#2e2e2e' : '#e0e0e0',
+    padding: 10,
+    borderRadius: 8,
+    flex: 1,
+  },
+  button: {
+    backgroundColor: 'orange',
+    padding: 10,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  assistantResponse: {
+    backgroundColor: colorScheme === 'dark' ? '#2b2b2b' : '#a8a8a8',
+    padding: 10,
+    borderRadius: 8,
+    alignItems: 'center',
+    alignSelf: 'flex-start',
+  },
+  userResponse: {
+    backgroundColor: colorScheme === 'dark' ? '#2b2b2b' : '#a8a8a8',
+    padding: 10,
+    borderRadius: 8,
+    alignItems: 'center',
+    alignSelf: 'flex-end',
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+});
+
 export default Chat;
