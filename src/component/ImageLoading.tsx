4f83d5fdb36157c1999a32d314422d58963b9791
FightFarewellFearless <faizakbar088@gmail.com>
2024-03-09 19:54:07 +0700

    (fix): improve performance a bit, and fix loading issue with image

--- src/component/ImageLoading.tsx
+++ src/component/ImageLoading.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import {
   View,
   ActivityIndicator,
@@ -6,12 +6,33 @@ import {
   ImageBackgroundProps,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import URL from 'url';
+import AnimeLocal from '../utils/animeLocalAPI';
 
 function ImageLoading(props: ImageBackgroundProps) {
+  let imageSourceUri = typeof props.source === 'object' && !Array.isArray(props.source) ? props.source.uri : props.source;
+  if (typeof props.source === 'object' && !Array.isArray(props.source) && props.source.uri?.includes('otakudesu')) {
+    const withoutDomain = URL.parse(props.source.uri);
+    imageSourceUri = withoutDomain.protocol + '//' + AnimeLocal.BASE.domain + withoutDomain.pathname;
+  }
+
   const lastLink = useRef(props.source);
 
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  const onLoadStart = useCallback(() => {
+    setLoading(true);
+  }, []);
+
+  const onLoadEnd = useCallback(() => {
+    setLoading(false);
+  }, []);
+
+  const onError = useCallback(() => {
+    setError(true);
+    setLoading(false);
+  }, []);
 
   if (lastLink.current !== props.source) {
     // reset the state. useful on recycledView
@@ -23,13 +44,11 @@ function ImageLoading(props: ImageBackgroundProps) {
   return (
     <ImageBackground
       {...props}
+      source={(typeof props.source === 'object' && 'uri' in props.source && typeof imageSourceUri === 'string') ? { ...props.source, uri: imageSourceUri } : props.source}
       style={[props.style, { overflow: 'hidden' }]}
-      onLoadStart={() => setLoading(true)}
-      onLoadEnd={() => setLoading(false)}
-      onError={() => {
-        setError(true);
-        setLoading(false);
-      }}>
+      onLoadStart={onLoadStart}
+      onLoadEnd={onLoadEnd}
+      onError={onError}>
       {props.children}
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         {loading && <ActivityIndicator />}
