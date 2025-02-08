110cf538ebeead8305723c52fed04ad4f49ad156
FightFarewellFearless <faizakbar088@gmail.com>
2025-02-08 14:26:47 +0700

    chore: getting ready for react-compiler

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
     root: true,
     extends: '@react-native',
+    plugins: [
+        'eslint-plugin-react-compiler',
+    ],
     rules: {
+        'react-compiler/react-compiler': 'error',
         'react-native/no-inline-styles': 0,
         'react/no-unstable-nested-components': [
             'warn',
--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,25 @@
+const ReactCompilerConfig = {
+  target: '18', // '17' | '18' | '19'
+  sources: (filename) => {
+    return filename.includes('src/component');
+  },
+};
+
 module.exports = {
-  presets: ['babel-preset-expo'],
+  presets: [
+    ['babel-preset-expo', {
+      'react-compiler': ReactCompilerConfig
+    }]
+  ],
   // eslint-disable-next-line prettier/prettier
   plugins: [
     ['module:react-native-dotenv'],
     [
       "babel-plugin-inline-import",
       {
-          "extensions": [".md", ".txt"]
+        "extensions": [".md", ".txt"]
       }
-  ],
+    ],
     'react-native-reanimated/plugin',
   ],
 };
