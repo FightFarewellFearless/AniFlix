f7baafcd1fb078b71f63a8b93e737a7a31c58b01
FightFarewellFearless <faizakbar088@gmail.com>
2025-02-23 16:33:55 +0700

    fix: replace dynamic user agent with static string for better privacy

--- src/utils/deviceUserAgent.ts
+++ src/utils/deviceUserAgent.ts
@@ -1,3 +1,1 @@
-import DeviceInfo from 'react-native-device-info';
-
-export default DeviceInfo.getUserAgentSync();
+export default 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.0.0 Mobile Safari/537.36';
