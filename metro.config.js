0fb5c8bd7224dda13f18ef853f625d86fd049e2f
FightFarewellFearless <faizakbar088@gmail.com>
2025-03-15 13:54:10 +0700

    feat: Enable inline requires in Metro configuration

--- metro.config.js
+++ metro.config.js
@@ -9,6 +9,15 @@ const defaultConfig = getDefaultConfig(__dirname);
  * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {
+  transformer: {
+    async getTransformOptions() {
+      return {
+        transform: {
+          inlineRequires: true,
+        },
+      };
+    },
+  },
   resolver: {
     sourceExts: [...defaultConfig.resolver.sourceExts, 'md', 'cjs'],
   },
