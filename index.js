e605643f8907ace8cd30b9cd89230d52992421d0
FightFarewellFearless <faizakbar088@gmail.com>
2025-03-19 09:16:31 +0700

    refactor: Remove reanimated logger configuration from index.js

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ import { AppRegistry, Text } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-import { configureReanimatedLogger } from 'react-native-reanimated';
-
 require('moment/locale/id');
-
-configureReanimatedLogger({
-  strict: false,
-});
 
 Text.defaultProps = Text.defaultProps || {};
 Text.defaultProps.allowFontScaling = false;
