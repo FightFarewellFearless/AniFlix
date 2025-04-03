796d085ef517e6bbe7264075565c0da293349c0b
FightFarewellFearless <faizakbar088@gmail.com>
2025-04-03 09:00:42 +0700

    feat: add lazy loading message

--- src/component/misc/SuspenseLoading.tsx
+++ src/component/misc/SuspenseLoading.tsx
@@ -1,12 +1,14 @@
 import { Suspense } from 'react';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
+import useGlobalStyles from '../../assets/style';
 
 function Loading() {
+  const globalStyles = useGlobalStyles();
   return (
-    <ActivityIndicator
-      style={{ flex: 1, justifyContent: 'center', alignSelf: 'center' }}
-      size="large"
-    />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator size="large" />
+      <Text style={globalStyles.text}>Loading bundle...</Text>
+    </View>
   );
 }
 
