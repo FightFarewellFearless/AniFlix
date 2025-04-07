aa547d9c948a502049810fc038eae3a31b97185c
FightFarewellFearless <faizakbar088@gmail.com>
2025-04-07 13:25:48 +0700

    fix: issue with formData

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ import { AppRegistry, Text } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-import { fetch } from 'expo/fetch';
-global.fetch = fetch;
+import { fetch as expoFetch } from 'expo/fetch';
+global.RNFetch = fetch;
+global.fetch = expoFetch;
 
 require('moment/locale/id');
 
