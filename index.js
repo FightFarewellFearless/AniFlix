428f3adb39b6a36c590284543509695994645bba
FightFarewellFearless <faizakbar088@gmail.com>
2025-04-07 12:47:17 +0700

    feat: use expo fetch

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import { AppRegistry, Text } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+import { fetch } from 'expo/fetch';
+global.fetch = fetch;
+
 require('moment/locale/id');
 
 Text.defaultProps = Text.defaultProps || {};
