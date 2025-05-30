8d001a1e95c638f0b3f288ad6d0ce11e2e8f0234
FightFarewellFearless <faizakbar088@gmail.com>
2025-05-30 08:54:00 +0700

    feat: handle gesture tap manually because <Pressable> has an issue

--- src/component/misc/TouchableOpacityRNGH.tsx
+++ src/component/misc/TouchableOpacityRNGH.tsx
@@ -1,15 +1,49 @@
-import { StyleSheet } from 'react-native';
-import { PressableProps, Pressable } from 'react-native-gesture-handler';
+import { useMemo } from 'react';
+import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
+import { Gesture, GestureDetector, Pressable } from 'react-native-gesture-handler';
+import {
+  default as Reanimated,
+  runOnJS,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
-export function TouchableOpacity(props: PressableProps): ReturnType<typeof Pressable> {
-  const style = props.style ?? {};
+type Props = {
+  children: React.ReactNode;
+  style: StyleProp<ViewStyle>;
+  onPress: () => void;
+  disabled: boolean;
+};
+
+export function TouchableOpacity(props: Partial<Props>): ReturnType<typeof Pressable> {
+  const opacity = useSharedValue(1);
+  const sigleTaps = useMemo(
+    () =>
+      Gesture.Tap()
+        .enabled(!props.disabled)
+        .onBegin(() => {
+          opacity.set(withTiming(0.5, { duration: 100 }));
+        })
+        .onFinalize(() => {
+          opacity.set(withTiming(1, { duration: 100 }));
+        })
+        .onEnd(() => {
+          if (props.onPress) {
+            runOnJS(props.onPress)();
+          }
+        }),
+    [opacity, props.disabled, props.onPress],
+  );
   return (
-    <Pressable
-      {...props}
-      style={({ pressed }) => ({
-        opacity: pressed ? 0.5 : 1,
-        ...(typeof style === 'object' ? StyleSheet.flatten(style) : {}),
-      })}
-    />
+    <GestureDetector gesture={sigleTaps}>
+      <Reanimated.View style={[StyleSheet.flatten(props.style), { opacity }]}>
+        {/* 
+          TODO: The current implementation somehow gives an error
+          [Reanimated] Tried to modify key `current` of an object which has been already passed to a worklet 
+          Possibly related to react-native-dropdown-element's ref
+        */}
+        {props.children}
+      </Reanimated.View>
+    </GestureDetector>
   );
 }
