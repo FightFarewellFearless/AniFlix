8c0e17cea2e7a083736a6b58fef1523efb7186f9
FightFarewellFearless <faizakbar088@gmail.com>
2025-06-20 10:47:33 +0700

    feat: use expo's registerRootComponent

--- android/app/src/main/java/com/pirles/anime/MainActivity.kt
+++ android/app/src/main/java/com/pirles/anime/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "AniFlix"
+  override fun getMainComponentName(): String = "main"
 
   override fun onCreate(savedInstanceState: Bundle?) {
     SplashScreenManager.registerOnActivity(this)
--- index.js
+++ index.js
@@ -2,13 +2,13 @@
  * @format
  */
 import 'react-native-gesture-handler';
-import { AppRegistry, Text } from 'react-native';
+import { registerRootComponent } from 'expo';
+import { Text } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
 
 require('moment/locale/id');
 
 Text.defaultProps = Text.defaultProps || {};
 Text.defaultProps.allowFontScaling = false;
 
-AppRegistry.registerComponent(appName, () => App);
+registerRootComponent(App);
