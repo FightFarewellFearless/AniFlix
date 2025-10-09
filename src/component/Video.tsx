446d48f9d213467407e63dda9bc397db8431c73c
FightFarewellFearless <faizakbar088@gmail.com>
2025-10-09 16:55:52 +0700

    fix: webview video dimension

--- src/component/Video.tsx
+++ src/component/Video.tsx
@@ -728,7 +728,19 @@ function Video(props: Props) {
               source={{
                 ...(data.resolution?.includes('lokal')
                   ? {
-                      html: `<iframe src="${data.streamingLink}" style="width: 100vw; height: 100vh;" allowFullScreen>`,
+                      html: `
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+<body>
+  <iframe
+    src="${data.streamingLink}"
+    style="width: 100vw; height: 100vh;"
+    allowFullScreen
+  ></iframe>
+</body>`,
                     }
                   : { uri: data.streamingLink }),
                 baseUrl: `https://${url.parse(data.streamingLink).host}`,
