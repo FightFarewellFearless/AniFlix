9fd67f572d95b7e0bf402f67ba9e0e79399417a6
FightFarewellFearless <faizakbar088@gmail.com>
2026-02-19 14:34:49 +0700

    fix(ImageLoading): indicator position

--- src/component/misc/ImageLoading.tsx
+++ src/component/misc/ImageLoading.tsx
@@ -86,6 +86,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
 });
 
